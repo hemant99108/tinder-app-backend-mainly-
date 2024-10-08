e9bfea11c65a2a92ab85b0a378b2148271013f31
Hemant <kumarhemant99108@gmail.com>
2024-10-08 08:03:34 +0530

    exploring about delete and patch api with mongodb

--- backend/src/app.js
+++ backend/src/app.js
@@ -22,8 +22,66 @@ app.post("/signup",async(req,res)=>{
 });
 
 
+app.get('/feed',async(req,res)=>{
 
 
+    try {
+        const users=await User.find({});
+        res.send(users);
+    } catch (error) {
+        res.status(500).send('error fetching user collection');
+    }
+})
+
+app.get('/user',async(req,res)=>{
+    const useremail=req.body.email;
+
+    try {
+        const user=await User.find({emailId:useremail});
+
+        if(!user){
+            res.status(404).json({message:'user not found'});
+        }
+        else{
+            res.send(user);
+        }
+    } catch (error) {
+        res.status(500).send("user with email does not exists");
+    }
+})
+
+
+app.delete('/user',async(req,res)=>{
+    const{ userid,email}=req.body;
+
+    try {
+        await User.findOneAndDelete(userid);                                            
+        console.log("user deletion successfully completed");
+        res.send('user deleted successfully');
+    } catch (error) {
+        console.log('error while deletion of the user');
+        res.status(500).send("user with emailId  does not exists");
+    }
+});
+
+app.patch('/user',async(req,res)=>{
+    const {userid}=req.body; 
+    const data=req.body;
+    
+    try {
+        
+        const user=await User.findByIdAndUpdate(userid,data,{returnDocumnent:'before'});
+        await user.save();
+
+        res.send(user);
+        console.log('user updated successfully',user);
+
+    }
+    catch (error) {
+        res.status(500).send("Error updating user");
+        console.log('error updating user');
+    }
+})
 
 
 
