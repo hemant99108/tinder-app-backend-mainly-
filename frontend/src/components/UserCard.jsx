0d8dc49409789142332c8619131db2693da2265d
Hemant <kumarhemant99108@gmail.com>
2024-11-29 16:04:56 +0530

    building feed page

--- frontend/src/components/UserCard.jsx
+++ frontend/src/components/UserCard.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
 import axios from "axios";
 import { BASE_URL } from "../utils/constants";
 import { useDispatch } from "react-redux";
@@ -14,13 +16,38 @@ function UserCard({user}) {
             );
 
             dispatch(removeUserFromFeed(userId));
-        } catch (error) {}
+        } catch (error) {
+          console.log(error);
+        }
     }
 
 
   return (
-    <div>UserCard</div>
+    <div className="card bg-base-300 w-96 shadow-xl">
+      <figure>
+        <img src={user.photoUrl} alt="photo" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{firstName + " " + lastName}</h2>
+        {age && gender && <p>{age + ", " + gender}</p>}
+        <p>{about}</p>
+        <div className="card-actions justify-center my-4">
+          <button
+            className="btn btn-primary"
+            onClick={() => handleSendRequest("ignored", _id)}
+          >
+            Ignore
+          </button>
+          <button
+            className="btn btn-secondary"
+            onClick={() => handleSendRequest("interested", _id)}
+          >
+            Interested
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default UserCard
+export default UserCard;  
